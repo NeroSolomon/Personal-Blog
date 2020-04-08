3a44670a2732c4f6e1801223a333f12fd844d4c5
mingyang.tan <mingyang.tan@mobvista.com>
2020-04-08 20:02:19 +0800

    update

--- css/css的一些知识点.md
+++ css/css的一些知识点.md
@@ -6,3 +6,21 @@
 ### Reset/Normalize：
 Reset：将所有浏览器的默认样式都统一化，注重的是跨浏览器统一样式，用户还要自行添加一些默认样式。
 Normalize：会根据各个浏览器的不同保留有用的浏览器特色样式，修复浏览器的一些BUG，更注重易用性。
+
+### 更改滚动条样式
+```css
+::-webkit-scrollbar{
+  width: 5px;
+  height: 5px;
+}
+
+::-webkit-scrollbar-thumb{
+  border-radius: 1em;
+  background-color: rgba(50,50,50,.3);
+}
+
+::-webkit-scrollbar-track{
+  border-radius: 1em;
+  background-color: rgba(50,50,50,.1);
+}
+```
---
+++ js/js例子/递归/字符串的排列.js
@@ -0,0 +1,23 @@
+function Permutation(str){
+  let res=[]
+  if(str.length==1){
+    res.push(str)
+  }else{
+    let obj=[]
+    for(let i=0;i<str.length;i++){
+      let c=str[i]
+      if(!obj[c]){
+        let newStr=str.slice(0,i)+str.slice(i+1)
+        let l=Permutation(newStr)
+        for(var j=0;j<l.length;j++){
+          res.push(c+l[j])
+        }
+        obj[c]=true
+      }
+    }
+  }
+  return res
+}
+
+const result = Permutation('abc');
+console.log(Permutation('abc'));
