75c10d70041cc78c9b9f695f7d475d217fa34b4f
tanmingyang <tanmingyang@corp.netease.com>
2021-03-11 20:52:22 +0800

    feat: 完成阅读createStore

---
+++ react/源码解读/redux/redux/src/index.js
@@ -0,0 +1,37 @@
+// 创建store
+import createStore from './createStore'
+// 合并reducer
+import combineReducers from './combineReducers'
+import bindActionCreators from './bindActionCreators'
+// 扩展redux功能
+import applyMiddleware from './applyMiddleware'
+import compose from './compose'
+import warning from './utils/warning'
+
+/*
+* This is a dummy function to check if the function name has been altered by minification.
+* If the function has been minified and NODE_ENV !== 'production', warn the user.
+*/
+function isCrushed() {}
+
+if (
+  process.env.NODE_ENV !== 'production' &&
+  typeof isCrushed.name === 'string' &&
+  isCrushed.name !== 'isCrushed'
+) {
+  warning(
+    'You are currently using minified code outside of NODE_ENV === \'production\'. ' +
+    'This means that you are running a slower development build of Redux. ' +
+    'You can use loose-envify (https://github.com/zertosh/loose-envify) for browserify ' +
+    'or DefinePlugin for webpack (http://stackoverflow.com/questions/30030031) ' +
+    'to ensure you have the correct code for your production build.'
+  )
+}
+
+export {
+  createStore,
+  combineReducers,
+  bindActionCreators,
+  applyMiddleware,
+  compose
+}
