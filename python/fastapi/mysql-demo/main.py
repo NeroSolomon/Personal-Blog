7c508e2a75d0365bc29e5dce792571eafccafb11
tanmingyang <tanmingyang@corp.netease.com>
2022-05-17 21:11:24 +0800

    feat: fastapi链接项目

---
+++ python/fastapi/mysql-demo/main.py
@@ -0,0 +1,76 @@
+# https://blog.csdn.net/weixin_45248616/article/details/124184258
+from fastapi import FastAPI
+
+app = FastAPI()
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# 配置数据库地址：数据库类型+数据库驱动名称://用户名:密码@机器地址:端口号/数据库名
+engine = create_engine("mysql+pymysql://root:Hello1024@127.0.0.1:3306/trip_django")
+
+# 把当前的引擎绑定给这个会话；
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# 实例化
+session = Session()
+
+# declarative_base类维持了一个从类到表的关系，通常一个应用使用一个Base实例，所有实体类都应该继承此类对象
+from sqlalchemy.ext.declarative import declarative_base
+# 实例化
+Base = declarative_base()
+
+from sqlalchemy import Column, String, Integer
+
+# 创建数据库
+class User(Base):
+  # 表名
+  __tablename__ = 'tbuser'
+  # 字段
+  # primary_key=True 设为主键
+  userid = Column(Integer, primary_key=True)
+  username = Column(String(255))
+
+  # 构造函数
+  def __init__(self, useid, username):
+    self.userid = useid
+    self.username = username
+  
+  # 打印
+  def __str__(self):
+    return "id：%s, name：%s" % (str(self.userid) ,self.username)
+
+  # 定义返回结果
+  def to_dict(self):
+    return {
+      "userid": self.userid,
+      "username": self.username
+    }
+
+# 在数据库中生成表
+Base.metadata.create_all(bind=engine)
+
+from pydantic import BaseModel
+# 定义数据模型
+class CreateUser(BaseModel):
+  userid: int
+  username: str
+
+# 添加单个
+@app.post("/user/addUser")
+async def InsertUser(user: CreateUser):
+  try:
+    dataUser = User(useid=user.userid, username=user.username)
+    session.add(dataUser)
+    session.commit()
+    session.close()
+  except ArithmeticError:
+    return {"code":"0002","message":"数据库异常"}
+  return {"code":"0000","message":"添加成功"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app=app, host="127.0.0.1", port=8000, debug=True)
