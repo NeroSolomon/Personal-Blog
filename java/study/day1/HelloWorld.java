8e3e79d2a84799f1cd51633bc7275a1f3ed46230
tanmingyang <tanmingyang@corp.netease.com>
2026-04-14 10:29:21 +0800

    feat: 更新笔记

---
+++ java/study/day1/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将输出字符串 Hello World
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // 输出 Hello World
+    }
+}
+
+// tanmingyang@oioioi day1 % javac HelloWorld.java
+// tanmingyang@oioioi day1 % java HelloWorld
